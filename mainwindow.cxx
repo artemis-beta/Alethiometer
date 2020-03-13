832cac99abdc8f993c440e230e34e875b5c11a4e
artemis-beta <krizar312@gmail.com>
2020-03-13 18:01:05 +0000

    Alethiometer now works

--- mainwindow.cxx
+++ mainwindow.cxx
@@ -5,7 +5,10 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    qsrand(time(NULL));
     this->setGeometry(0, 0, 768, 768);
+    connect(timer_1, SIGNAL(timeout()), this, SLOT(setNewPosition()));
+    connect(timer_2, SIGNAL(timeout()), this, SLOT(updateNeedle()));
 }
 
 MainWindow::~MainWindow()
@@ -13,7 +16,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::paintEvent(QPaintEvent* event)
+void MainWindow::paintEvent(QPaintEvent*)
 {
     QPainter* _painter = new QPainter(this);
     const double offset = 0.5*(resolution[1]-aleth_dimensions[1]);
@@ -37,5 +40,71 @@ void MainWindow::wheelEvent(QWheelEvent * event)
             update();
         }
     }
+}
+void MainWindow::mousePressEvent(QMouseEvent*)
+{
+    predict();
+}
+
+void MainWindow::updateNeedle()
+{
+    timer_1->stop();
+    if(needle_position == _results[needle_target_index])
+    {
+        timer_2->stop();
+        if(needle_target_index > 1)
+        {
+            timer_1->stop();
+            needle_target_index = 0;
+            return;
+        }
+        needle_target_index += 1;
+        timer_1->start(1000);
+        return;
+    }
+    needle_position = (needle_position < 36) ? needle_position+direction : 0;
+    needle_position = (needle_position < 0) ? 36+needle_position : needle_position;
+    _arms[0]->setPosition(needle_position);
+
+
+    this->update();
+    qDebug() << _results[needle_target_index] << " " << needle_position;
+
+}
+
+void MainWindow::setNewPosition()
+{
+    if(_results[needle_target_index] > needle_position)
+    {
+       direction = (abs(_results[needle_target_index]-needle_position) > 18) ? -1 : 1;
+    }
+    else
+    {
+        direction = (abs(_results[needle_target_index]-needle_position) < 18) ? -1 : 1;
+    }
+    if(abs(_arms[0]->getPosition()-_results[needle_target_index]) < abs((36-_arms[0]->getPosition()+_results[needle_target_index])))
+    {
+        magnitude = abs(_arms[0]->getPosition()-_results[needle_target_index]);
+    }
+    else
+    {
+        magnitude = abs((36-_arms[0]->getPosition()+_results[needle_target_index]));
+    }
+    timer_2->start(100);
+}
+
+void MainWindow::predict()
+{
+    for(int i{0}; i < 3; ++i) _results[i] = qrand() % 36;
+    direction = (abs(needle_position-_results[needle_target_index]) < 18) ? 1 : -1;
+    if(abs(_arms[0]->getPosition()-_results[needle_target_index]) < abs((36-_arms[0]->getPosition()+_results[needle_target_index])))
+    {
+        magnitude = abs(_arms[0]->getPosition()-_results[needle_target_index]);
+    }
+    else
+    {
+        magnitude = abs((36-_arms[0]->getPosition()+_results[needle_target_index]));
+    }
+    timer_2->start(100);
 }
 
--- mainwindow.hxx
+++ mainwindow.hxx
@@ -1,3 +1,22 @@
+/*
+    Simulation of the Alethiometer present in the "His Dark Materials" trilogy by
+    Philip Pullman.
+    Copyright (C) 2020 Kristian Zarebski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef MAINWINDOW_HXX
 #define MAINWINDOW_HXX
 
@@ -7,6 +26,7 @@
 #include <QPainter>
 #include <QPaintEvent>
 #include <QDebug>
+#include <QTimer>
 
 #include "arm.hxx"
 
@@ -31,6 +51,10 @@ private:
               arm3_dimensions   = {38, 230},
               needle_dimensions = {40, 315};
     const QList<int> resolution = {1024, 768};
+    int needle_position = 0, magnitude = 0, needle_target_index=0, direction=0;
+    QList<int> _results = {0,0,0};
+    QTimer* timer_2 = new QTimer(this);
+    QTimer* timer_1 = new QTimer(this);
     const double centre_offset = -16;
     QIcon* _aleth_svg = new QIcon(":/media/images/alethiometer.svg");
     QMap<int, Arm*> _arms = {{1, new Arm(QString(":/media/images/arm1.svg"), resolution, arm1_dimensions, centre_offset)},
@@ -44,6 +68,12 @@ private:
                                                          };
     void paintEvent(QPaintEvent*);
     void wheelEvent(QWheelEvent*);
+    void mousePressEvent(QMouseEvent*);
     void setArm(QPainter*, QIcon*, int, int, int, int arm_offset=0);
+    void predict();
+
+public slots:
+    void updateNeedle();
+    void setNewPosition();
 };
 #endif // MAINWINDOW_HXX
